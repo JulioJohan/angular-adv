29f715a983d2771edca0bc5a66cfe8033d32fa51
JJ <JJ@fsw.com.mx>
2022-12-26 13:44:58 -0600

    fin seccion 4 - Router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    PagesComponent,
+    Grafica1Component,
+    ProgressComponent
+  ],
+  exports:[
+    DashboardComponent,
+    PagesComponent,
+    Grafica1Component,
+    ProgressComponent
+  ],
+  imports: [
+    CommonModule,
+    //requiero este modulo en los pages
+    SharedModule,
+    RouterModule
+  ]
+})
+export class PagesModule { }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   ]
 })
 export class ProgressComponent {
-
+  constructor() {
+    console.log("proggress");
+  }
+  
 }
